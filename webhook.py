ac0ade6aecfab7d0d835bb3684f0c409fe9c9394
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2024-11-23 21:57:49 -0500

    primer commit

--- webhook.py
+++ webhook.py
@@ -13,13 +13,14 @@ def webhook():
         # Verificación del webhook
         token = request.args.get('hub.verify_token')
         challenge = request.args.get('hub.challenge')
-        
+
         if token == VERIFY_TOKEN:
             return challenge, 200
         return "Token no válido", 403
 
     elif request.method == 'POST':
         # Procesar mensajes entrantes
+        print("procesando nueva solicitud.")
         data = request.get_json()
         print("Mensaje recibido:", data)
         # Enviar a message_handler para procesar
